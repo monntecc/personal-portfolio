a5eb4eeb20187cabb778edbc9be2de936296171c
Vladyslav Potapenko <contact@nylestroke.me>
2022-09-22 21:35:05 +0200

    landing page, header and main component + responsive

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,48 @@
+import React, {useState} from "react";
+import { Link, Navigate } from "react-router-dom";
+
+import { GitHub } from "@mui/icons-material";
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+
+import "./Header.sass";
+
+export const Header = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  return (
+    <div className="container">
+      <header>
+        <nav>
+          <Link to="/" className="nav-brand">
+            N
+          </Link>
+          <Link to="/overview">Overview</Link>
+          <Link to="/skills">Skills</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/contact">Contact</Link>
+          <div className="responsive" data-opened={menuOpened}>
+            <Link to="/overview">Overview</Link>
+            <Link to="/skills">Skills</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
+          </div>
+        </nav>
+        <a href='https://github.com/nylestroke?tab=repositories' target='_blank' rel='noreferrer'
+          className="button"
+        >
+          <GitHub />
+          <span>Repositories</span>
+        </a>
+        <div className="mobile-btn">
+          <span onClick={() => setMenuOpened(!menuOpened)} data-opened={!menuOpened}>
+            <MenuIcon />
+          </span>
+          <span onClick={() => setMenuOpened(!menuOpened)} data-opened={menuOpened}>
+            <CloseIcon />
+          </span>
+        </div>
+      </header>
+    </div>
+  );
+};
---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import LoginImage from "../../assets/img/login.png";
+import ArrowForward from "@mui/icons-material/ArrowForward";
+
+import "./Main.sass";
+import {Link} from "react-router-dom";
+
+export const Main = () => {
+  return (
+    <main>
+      <div className="hero">
+        <div className="hero__title">
+          <h2>Hi, they call me</h2>
+          <span>Vladyslav Potapenko!</span>
+        </div>
+        <div className="hero__overview">
+          I am a full-stack programmer, gamer, member of Enqluence Studios, and
+          student at TEB Edukacja
+        </div>
+        <Link to='/overview' className="hero__link">
+          <span>Go To Overview</span>
+          <div className="icon">
+            <ArrowForward />
+          </div>
+        </Link>
+        <div className="hero__introduction">
+          <div className="introduction__block">
+            <h2>PROJECTS</h2>
+            <span>4</span>
+          </div>
+          <div className="introduction__block">
+            <h2>EXPERIENCE</h2>
+            <span>1 yr</span>
+          </div>
+          <div className="introduction__block nationality">
+            <h2>NATIONALITY</h2>
+            <span>
+                <div>
+                    UKR
+                </div>
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="36"
+                  height="14"
+                >
+                  <rect width="18" height="7" fill="#005BBB" />
+                  <rect width="18" height="7" y="7" fill="#FFD500" />
+                </svg>
+              </div>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="hero__image">
+        <img src={LoginImage} alt="login" draggable={false}/>
+      </div>
+    </main>
+  );
+};
